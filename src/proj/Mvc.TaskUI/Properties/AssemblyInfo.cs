b1cffb7535be0c10fb58c3450bc61211d4700a0f
Oliver, Jonathan <joliver@localhost>
2010-08-09 19:26:50 -0600

    Setup basic project structure.

---
+++ src/proj/Mvc.TaskUI/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Mvc.TaskUI")]
+[assembly: AssemblyDescription("")]
+[assembly: Guid("ab45c04c-b7b7-476f-acd1-8d2023a4fcd1")]
