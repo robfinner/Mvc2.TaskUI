d4ae64c6b8e72cbca064e51e14733cc9e739f0de
Oliver, Jonathan <joliver@localhost>
2010-08-19 08:55:48 -0600

    Using implicit "this" variable; added <label for="" /> to error messages.

--- src/proj/Mvc.TaskUI/jquery.hijax.js
+++ src/proj/Mvc.TaskUI/jquery.hijax.js
@@ -1,26 +1,26 @@
 ﻿$(document).ready(function()
 {
 	var forms = $("form.hijax");
-	$.each(forms, function(i, form)
+	$.each(forms, function()
 	{
-		form.requestId = newGuid();
+		this.requestId = newGuid();
 
-		$(form).submit(function(event)
+		$(this).submit(function(event)
 		{
 			event.preventDefault();
 			event.stopPropagation();
 
-			if (form.onSubmit && false === form.onSubmit())
-				return false; // form.onSubmit must explictly return false to stop processing
+			if (this.onSubmit && false === this.onSubmit())
+				return false; // this.onSubmit must explictly return false to stop processing
 
-			if (form.onInit)
-				form.onInit();
+			if (this.onInit)
+				this.onInit();
 
-			form.hideInputErrors ? form.hideInputErrors() : hideInputErrors(form);
+			this.hideInputErrors ? this.hideInputErrors() : hideInputErrors(this);
 
-			var url = (form.action || window.location).toString();
-			url += (url.indexOf("?") < 0 ? "?" : "&") + "RequestId=" + form.requestId;
-			ajax(form, url, 0);
+			var url = (this.action || window.location).toString();
+			url += (url.indexOf("?") < 0 ? "?" : "&") + "RequestId=" + this.requestId;
+			ajax(this, url, 0);
 		});
 	});
 	function newGuid()
@@ -32,7 +32,6 @@
 				return v.toString(16);
 			}).toUpperCase();
 	}
-
 	function ajax(form, url, attempt)
 	{
 		if (form.onStatus)
@@ -101,11 +100,11 @@
 			return form.onInputErrors();
 
 		var summary = "";
-		$.each(errors, function(i, error)
+		$.each(errors, function()
 		{
-			var element = $(":input[name$=" + error.Property + "]", form);
-			element.addClass("input-validation-error").attr("_title", element.attr("title")).attr("title", error.Message);
-			summary += "<li>" + error.Message + "</li>";
+			var element = $(":input[name$=" + this.Property + "]", form);
+			element.addClass("input-validation-error").attr("_title", element.attr("title")).attr("title", this.Message);
+			summary += "<li><label for=\"" + this.Property + "\">" + this.Message + "</label></li>";
 		});
 
 		$(".hijaxSummary", form).html("<ul>" + summary + "</ul>").show();
@@ -113,10 +112,10 @@
 	function hideInputErrors(form)
 	{
 		$(".hijaxSummary", form).hide().html("");
-		$(":input", form).each(function(i, element)
+		$(":input", form).each(function()
 		{
-			element = $(element);
-			element.removeClass("input-validation-error").attr("title", element.attr("_title"));
+			$this = $(this);
+			$this.removeClass("input-validation-error").attr("title", $this.attr("_title"));
 		});
 	}
 });
